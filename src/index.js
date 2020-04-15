366c91b7fc208beddc6441bb48511e01c5d66107
Selma Datagirl <datagirlcmr@gmail.com>
2020-04-15 16:31:19 -0500

    worked on the dom elements

--- src/index.js
+++ src/index.js
@@ -1,26 +1,44 @@
 import './styles/style.css';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'domElements' from 'domelts';
 
-async function getWeather() {
-    const response = await fetch ("https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=a65bf913c63cc981687c4ea80470725d", {mode: 'cors'})
+const domElements = () => {
+
+    const formSubmit = document.querySelector('#imgsubmit');  
+    
+
+    const ctryName = document.querySelector('#ctry');
+    const toggler = document.querySelector('#toggle');
+    const currentDate = document.querySelector('#date');
+    
+    currentDate.textContent = new Date().toUTCString();
+}
+
+const getLocation = () => {
+    const searchIcon = document.querySelector('.search-icon');
+    const getInput = document.querySelector('#location');
+    searchIcon.addEventListener('click', () => {
+        const inputValue = getInput.value;
+        getWeather(inputValue);
+    })
+   
+}
+
+
+async function getWeather(value) {
+    const response = await fetch ("https://api.openweathermap.org/data/2.5/weather?q="+ value + "&APPID=a65bf913c63cc981687c4ea80470725d", {mode: 'cors'})
     const weatherdata = await response.json();
 
     console.log(weatherdata);
+    
 
     const {lon, lat} = weatherdata.coord;
-
     const {temp, feels_like} = weatherdata.main;
-
     const {speed, deg} = weatherdata.wind;
-
     const {country} = weatherdata.sys;
-
     const name = weatherdata.name;
-
-    console.log(name);
+     console.log(name);
 
 }
-   
 domElements();
+getLocation();
