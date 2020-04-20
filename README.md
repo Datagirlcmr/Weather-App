9e8dcb5c58b6cf469a40329550ea5a49526078f0
Selma Datagirl <datagirlcmr@gmail.com>
2020-04-20 11:56:45 -0500

    updated the readme

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# Weather-App
- a weather forecast site using the weather API
+# Weather App
+This project involves implementing aynchronous javascript via aync/await and Promises to build a weather forecase app from the openweather site API.
+
+## Live Demo Link
+
+[Live Demo Link](https://raw.githack.com/Datagirlcmr/Weather-App/feature/dist/index.html)
+
+=======
+
+## Live Version
+
+<img src='src/images/weatherapp.png' alt='weather app'>
+
+## Get Started
+
+- Download or clone this [repo](https://github.com/Datagirlcmr/Weather-App) here.
+- Install webpack and other project dependencies by running
+
+        npm install
+
+- Run 
+
+     npm run dev
+
+to watch your files
+- Open your index.html file and input the location where you need weather data for.
+
+## Built With
+
+- HTML, CSS, Bootstrap, Javascript
+- OpenWeather API
+- Webpack for bundling
+- Vscode for coding
+- ESLint for Validation
+
+
+For more information, see the
+[*The Odin Project*](https://www.theodinproject.com/courses/javascript/lessons/weather-app).
+
+## Author
+
+👤 **Selma Ndi**
+
+- Github: [@githubhandle](https://github.com/Datagirlcmr)
+- Twitter: [@twitterhandle](https://twitter.com/SelmaNdi)
+- Linkedin: [linkedin](https://www.linkedin.com/in/selma-ndi-datagirl-imba-8976ab32/)
+
+
+Contributions, issues and feature requests are welcome! 
+
+Feel free to check the [issues page](https://github.com/Datagirlcmr/Weather-App/issues).
--- src/domelts.js
+++ src/domelts.js
@@ -1,78 +1,72 @@
 const getDate = () => {
-    const currentDate = document.querySelector('#date');
-    currentDate.textContent = new Date().toUTCString();
-}
-
-
-const getLocation = () => {
-    const searchIcon = document.querySelector('.search-icon');
-    const getInput = document.querySelector('#location');
-
-
-    getInput.addEventListener("keyup", function (event) {
-        if (event.keyCode === 13) {
-            event.preventDefault();
-            searchIcon.click();
-        }
-    });
-
-    searchIcon.addEventListener('click', () => {
-        const inputValue = getInput.value;
-        getWeather(inputValue);
-    });
-}
+  const currentDate = document.querySelector('#date');
+  currentDate.textContent = new Date().toUTCString();
+};
 
 async function getWeather(value = 'Bamenda') {
-    const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + value + "&APPID=a65bf913c63cc981687c4ea80470725d", { mode: 'cors' })
-    const weatherdata = await response.json();
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${value}&APPID=a65bf913c63cc981687c4ea80470725d`, { mode: 'cors' });
+  const weatherdata = await response.json();
 
-    //console.log(weatherdata);
+  const [weather] = weatherdata.weather;
+  const { main } = weather;
+  const { temp, feels_like, humidity } = weatherdata.main;
+  const { speed } = weatherdata.wind;
+  const { country } = weatherdata.sys;
+  const { name } = weatherdata;
 
-    //const {lon, lat} = weatherdata.coord;
-    const [weather] = weatherdata.weather;
-    const { main, description } = weather;
-    let { temp, feels_like, humidity } = weatherdata.main;
-    const { speed, deg } = weatherdata.wind;
-    const { country } = weatherdata.sys;
-    const name = weatherdata.name;
+  const ctryName = document.querySelector('#ctry');
+  ctryName.textContent = `${name},  ${country}`;
 
-    const ctryName = document.querySelector('#ctry');
-    ctryName.textContent = name + ',  ' + country;
+  const tempCelcius = (temp - 273).toFixed(2);
+  const tempFahrenheit = ((tempCelcius * 1.8000) + 32).toFixed(2);
 
-    const tempCelcius = (temp - 273).toFixed(2);
-    const tempFahrenheit = ((tempCelcius * 1.8000) + 32).toFixed(2);
+  const feelsCelcius = (feels_like - 273).toFixed(2);
+  const feelsFahrenheit = ((feelsCelcius * 1.8000) + 32).toFixed(2);
 
-    const feelsCelcius = (feels_like - 273).toFixed(2);
-    const feelsFahrenheit = ((feelsCelcius * 1.8000) + 32).toFixed(2);
+  const temperature = document.querySelector('.temp');
+  temperature.textContent = `${tempCelcius} °C`;
 
-    const temperature = document.querySelector('.temp');
+  const feelsLike = document.querySelector('.feels-like');
+  feelsLike.textContent = ` but it feels like ${feelsCelcius} °C`;
+
+  const tempC = document.querySelector('#toggleC');
+  tempC.addEventListener('click', () => {
     temperature.textContent = `${tempCelcius} °C`;
+    feelsLike.textContent = `but it feels like ${feelsCelcius} °C`;
+  });
 
-    const tempC = document.querySelector('#toggleC');
-    tempC.addEventListener('click', () => {
-        temperature.textContent = `${tempCelcius} °C`;
-        feelsLike.textContent = `but it feels like ${feelsCelcius} °C`;
-    })
+  const tempF = document.querySelector('#toggleF');
+  tempF.addEventListener('click', () => {
+    temperature.textContent = `${tempFahrenheit} °F`;
+    feelsLike.textContent = `but it feels like ${feelsFahrenheit} °F`;
+  });
 
-    const tempF = document.querySelector('#toggleF');
-    tempF.addEventListener('click', () => {
-        temperature.textContent = `${tempFahrenheit} °F`;
-        feelsLike.textContent = `but it feels like ${feelsFahrenheit} °F`;
-    })
+  const getHumid = document.querySelector('#humid-w');
+  getHumid.textContent = `${humidity} %`;
 
-    const feelsLike = document.querySelector('.feels-like');
-    feelsLike.textContent = ` but it feels like ${feelsCelcius} °C`;
+  const getDesc = document.querySelector('#main-desc');
+  getDesc.textContent = `Mostly ${main}`;
 
-    const getHumid = document.querySelector('#humid-w');
-    getHumid.textContent = `${humidity} %`;
-
-    const getDesc = document.querySelector('#main-desc');
-    getDesc.textContent = `Mostly ${main}`;
-    //console.log(getDesc);
-
-    const getWind = document.querySelector('#wind-deg');
-    getWind.textContent = `${speed} mph`;
-
+  const getWind = document.querySelector('#wind-deg');
+  getWind.textContent = `${speed} mph`;
 }
+
+const getLocation = () => {
+  const searchIcon = document.querySelector('.search-icon');
+  const getInput = document.querySelector('#location');
+
+
+  getInput.addEventListener('keyup', (event) => {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      searchIcon.click();
+    }
+  });
+
+  searchIcon.addEventListener('click', () => {
+    const inputValue = getInput.value;
+    getWeather(inputValue);
+  });
+};
 
 export { getLocation, getWeather, getDate };
